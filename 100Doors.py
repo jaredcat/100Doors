093971d1fc4a93830cb0e328ba8a277d6022c85c
Jared Kotoff <3268576+jaredkotoff@users.noreply.github.com>
2023-10-30 12:49:40 -0700

    Run restyled.io against repo (#4)

--- 100Doors.py
+++ 100Doors.py
@@ -1,21 +1,36 @@
 import random
 
 # Simply picks the winning door at random
+
+
 def door_picker():
-    winner = random.randrange(1, doors+1)
+    """ """
+    winner = random.randrange(1, doors + 1)
     return winner
 
-# This opens all the other doors and allows the user to swich or stay
+
+# This opens all the other doors and allows the user to switch or stay
+
+
 def door_opener(choice, winner, switch, enable_auto):
+    """
+
+    :param choice:
+    :param winner:
+    :param switch:
+    :param enable_auto:
+
+    """
     if enable_auto == "n":
         switch = None
     if choice == winner:
-        closed_door = random.randrange(1, doors+1)
+        closed_door = random.randrange(1, doors + 1)
         while closed_door == winner:
-            closed_door = random.randrange(1, doors+1)
+            closed_door = random.randrange(1, doors + 1)
     else:
         closed_door = choice
-    print("I have opened all but doors " + str(closed_door) + " and " + str(winner))
+    print("I have opened all but doors " + str(closed_door) + " and " +
+          str(winner))
     if enable_auto == "n":
         while not (switch == "y" or switch == "n"):
             switch = input("Would you like to switch?(y\\n): ").lower()
@@ -26,8 +41,18 @@ def door_opener(choice, winner, switch, enable_auto):
             choice = winner
     return choice, switch
 
+
 # This is the end game. Displays if the player won or lost
+
+
 def show_winner(choice, winner, switch):
+    """
+
+    :param choice:
+    :param winner:
+    :param switch:
+
+    """
     if switch == "n":
         print("You did not switch and you ", end="")
     else:
@@ -39,35 +64,70 @@ def show_winner(choice, winner, switch):
         print("lost.")
         return 0
 
+
 # Calculates the amount of games won vs played and your % of wins
+
+
 def show_rate(wins, games):
+    """
+
+    :param wins:
+    :param games:
+
+    """
     rate = wins / games
     print("\n" + str(wins) + " wins of " + str(games) + " games")
-    print("You are winning " + str(rate*100) + "% of the time.\n\n")
+    print("You are winning " + str(rate * 100) + "% of the time.\n\n")
+
 
 def shuffle_boxes():
-    prisoners_numbers = [x for x in range(0,100)]
-    boxes={}
+    """ """
+    prisoners_numbers = [x for x in range(0, 100)]
+    boxes = {}
     random.shuffle(prisoners_numbers)
-    for i,p in enumerate(prisoners_numbers):
+    for i, p in enumerate(prisoners_numbers):
         boxes[i] = p
     return boxes
 
-def random_strategy(prisoner_number,boxes, number_loop):
-    for k in range(0,50):
+
+def random_strategy(prisoner_number, boxes, number_loop):
+    """
+
+    :param prisoner_number:
+    :param boxes:
+    :param number_loop:
+
+    """
+    for k in range(0, 50):
         if prisoner_number == boxes[k]:
             return True
     return False
 
+
 def try_strategy(boxes, strategy, number_loops):
+    """
+
+    :param boxes:
+    :param strategy:
+    :param number_loops:
+
+    """
     n_correct = 0
-    for i in range(0,100):
+    for i in range(0, 100):
         correct = strategy(i, boxes, number_loops)
         if correct:
             n_correct += 1
     return n_correct
 
-def loop_strategy(prisoner_number,boxes,number_loops):
+
+def loop_strategy(prisoner_number, boxes, number_loops):
+    """
+
+    :param prisoner_number:
+    :param boxes:
+    :param number_loops:
+
+    """
     next_box = prisoner_number
     for x in range(number_loops):
         if boxes[next_box] == prisoner_number:
@@ -75,7 +135,15 @@ def loop_strategy(prisoner_number,boxes,number_loops):
         next_box = boxes[next_box]
     return False
 
-def prisoners_simulation(total_games, number_loops,strategy):
+
+def prisoners_simulation(total_games, number_loops, strategy):
+    """
+
+    :param total_games:
+    :param number_loops:
+    :param strategy:
+
+    """
     wins = 0
     results = {}
     num_sim = total_games
@@ -87,15 +155,15 @@ def prisoners_simulation(total_games, number_loops,strategy):
         elif strategy == 2:
             n_correct = try_strategy(boxes, loop_strategy, number_loops)
             results[sim] = n_correct
-     
+
     for sim in range(num_sim):
         if results[sim] == 100:
             wins = wins + 1
     return wins
 
-# Sorry for the mess
-# Theres cleaner ways to made this main but I got tired
+
 def main():
+    """ """
     global doors
     doors = "0"
     wins = 0
@@ -115,7 +183,8 @@ def main():
             doors = input("How many doors would you like to play with? ")
         doors = int(doors)
         while not (enable_auto == "y" or enable_auto == "n"):
-                enable_auto = input("Would you like to see autoplay? (y\\n): ").lower()
+            enable_auto = input(
+                "Would you like to see autoplay? (y\\n): ").lower()
         if enable_auto == "y":
             while not (switch == "y" or switch == "n"):
                 switch = input("Always switch doors? (y\\n): ")
@@ -124,52 +193,59 @@ def main():
         while keep_playing == "y":
             choice = "0"
             if enable_auto == "y":
-                choice = str(random.randrange(1, doors+1))
-            print("There are " + str(doors) + " doors in front of you.\nOne contains a prize.\n")
+                choice = str(random.randrange(1, doors + 1))
+            print("There are " + str(doors) +
+                  " doors in front of you.\nOne contains a prize.\n")
             if enable_auto == "n":
-                while not (choice.isdigit() and 0 < int(choice) < doors+1):
+                while not (choice.isdigit() and 0 < int(choice) < doors + 1):
                     choice = input("Pick one: ")
             winner = door_picker()
-            choice, switch = door_opener(int(choice), winner, switch, enable_auto)
+            choice, switch = door_opener(int(choice), winner, switch,
+                                         enable_auto)
             wins += show_winner(int(choice), winner, switch)
             games += 1
             show_rate(wins, games)
             if enable_auto == "n":
                 keep_playing = None
                 while not (keep_playing == "y" or keep_playing == "n"):
-                    keep_playing = input("Would you like to keep playing? (y\\n): ").lower()
+                    keep_playing = input(
+                        "Would you like to keep playing? (y\\n): ").lower()
             elif int(total_games) == games:
                 keep_playing = "n"
-    
+
     elif choice == 2:
         while not (total_games.isdigit() and 0 < int(total_games)):
-                total_games = input("How many games?: ")
+            total_games = input("How many games?: ")
         total_games = int(total_games)
-        
-        
+
         strategy = "0"
-        print("\nChoose a strategy you want to simulate the \"100 Prisoners Problem\":")
+        print(
+            '\nChoose a strategy you want to simulate the "100 Prisoners Problem":'
+        )
         print("1. Random Strategy")
         print("2. Loop Strategy")
-        options = [1,2]
+        options = [1, 2]
         while not (strategy.isdigit() and int(strategy) in options):
-                strategy = input("Select the strategy: ")
+            strategy = input("Select the strategy: ")
         strategy = int(strategy)
-        
+
         if strategy == 1:
             print("Simulating....")
-            prisoners_wins = prisoners_simulation(total_games,0,strategy)
+            prisoners_wins = prisoners_simulation(total_games, 0, strategy)
             show_rate(prisoners_wins, total_games)
         elif strategy == 2:
             number_loops = input("Enter the number of loops? (default: 50): ")
             number_loops = int(number_loops or 50)
-            print("You have entered " + str(number_loops) +" number of loops.")
+            print("You have entered " + str(number_loops) +
+                  " number of loops.")
             print("Simulating...")
-            prisoners_wins = prisoners_simulation(total_games, number_loops,strategy)
+            prisoners_wins = prisoners_simulation(total_games, number_loops,
+                                                  strategy)
             show_rate(prisoners_wins, total_games)
-    
+
     else:
         print("\nWrong option selected!")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- README.md
+++ README.md
@@ -1,25 +1,31 @@
 # 100Doors
-A Monty Hall Simulator in python.
 
-There was a post about the Monty Hall problem and how it stumped the mathmatics world for years. After explaining it, people in the comments still firmly belived that the odds should be 50%. So, I wanted to see how easy I could build this 
-in python just to prove a point to myself and see the win rate with some big data.  
-A few hours later and I had a pretty robust simulator that confirms that you have n-1/n odds of winning when you switch 
-instead of staying with your initial guess!  
+## Monty Hall Simulator
+
+There was a post about the Monty Hall problem and how it stumped the mathmatics world for years. After explaining it, people in the comments still firmly belived that the odds should be 50%. So, I wanted to see how easy I could build this
+in python just to prove a point to myself and see the win rate with some big data.
+A few hours later and I had a pretty robust simulator that confirms that you have n-1/n odds of winning when you switch
+instead of staying with your initial guess!
 
 The name comes from the concept of extrapolating the initial scenario to a large number to make it easier to understand
-why odds shift to the unopened door and not equally between the two remaining closed doors.  
+why odds shift to the unopened door and not equally between the two remaining closed doors.
 
+### Instructions
 
-## Instructions:
-Requires Python (3.4 if you run into issues on lower versions)  
-Follow on screen instructions  
+Requires Python (3.4 if you run into issues on lower versions)
+Follow on screen instructions
 
-## Features: 
-The script features two play modes:  
-Auto play mode: If set to y then the game will play any amount of times with any amount of doors and either switch or stay 
-every time while randomly selecting a door. (Proves odds favor switching with big data)  
-Player mode: Allows the player to simulate being on a game show with as many doors as they want.  
-In player mode, a player gets to pick which door they want and  a winning one is randomly selected from the pool of 
-doors.  
-Then the player gets to pick whether they would like to switch or stay when the doors are opened.  
-The players win rate and percentage is displayed and carried with the player till they quit.  
+### Features
+
+The script features two play modes:
+Auto play mode: If set to y then the game will play any amount of times with any amount of doors and either switch or stay
+every time while randomly selecting a door. (Proves odds favor switching with big data)
+Player mode: Allows the player to simulate being on a game show with as many doors as they want.
+In player mode, a player gets to pick which door they want and a winning one is randomly selected from the pool of
+doors.
+Then the player gets to pick whether they would like to switch or stay when the doors are opened.
+The players win rate and percentage is displayed and carried with the player till they quit.
+
+## 100 Prisoners Simulator
+
+TODO
